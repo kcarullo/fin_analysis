42825b0d95245596cf4fe460f928ddbbf5c459ea
kcarullo <35302366+kcarullo@users.noreply.github.com>
2019-08-26 12:08:41 -0400

    Add files via upload

--- time_series_analysis/arima/arima_with_statsmodels.py
+++ time_series_analysis/arima/arima_with_statsmodels.py
@@ -81,3 +81,47 @@ adf_check(df['Seasonal Difference'].dropna())
 df['Seasonal First Difference'] = df['First Difference'] - df['First Difference'].shift(12)
 df['Seasonal First Difference'].plot()
 adf_check(df['Seasonal First Difference'].dropna())
+
+# Auto Correlation Plots 
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+fig_first = plot_acf(df['First Difference'].dropna())
+fig_seasonal_first = plot_acf(df['Seasonal First Difference'].dropna())
+
+#pacf
+fig_seasonal_pacf = plot_pacf(df['Seasonal First Difference'].dropna(),
+                              method='ywmle')
+
+# autocorrelation with pandas 
+from pandas.plotting import autocorrelation_plot
+autocorrelation_plot(df['Seasonal First Difference'].dropna())
+
+plot_acf(df['Seasonal First Difference'].dropna())
+plot_pacf(df['Seasonal First Difference'].dropna(), method='ywmle')
+
+# implementing ARIMA model
+from statsmodels.tsa.arima_model import ARIMA
+model = sm.tsa.statespace.SARIMAX(df['Milk in Pounds per Cow'], order=(0,1,0),
+                                  seasonal_order=(1,1,1,12))
+results = model.fit()
+print(results.summary())
+results.resid
+results.resid.plot()
+results.resid.plot(kind='kde')
+
+df['forecast'] = results.predict(start=150, end=168)
+df[['Milk in Pounds per Cow', 'forecast']].plot(figsize=(12,8))
+
+# add future rows so we can forecast for the future months
+from pandas.tseries.offsets import DateOffset
+# df.index[-1] is the last month in the index col you add a offset where months
+# is equal to x. The for loop is going to add the offset 1 month, 2, 3 and so on
+future_dates = [df.index[-1] + DateOffset(months=x) for x in range(1,24)]
+future_dates
+future_df = pd.DataFrame(index=future_dates, columns=df.columns)
+final_df = pd.concat([df, future_df])
+final_df.tail()
+
+final_df['forecast'] = results.predict(start=168, end=192)
+final_df.tail()
+
+final_df[['Milk in Pounds per Cow', 'forecast']].plot(figsize=(12,8))
