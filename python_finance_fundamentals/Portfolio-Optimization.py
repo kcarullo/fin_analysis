92680e7da0d559ffaea4374b78ede354445c430c
kcarullo <35302366+kcarullo@users.noreply.github.com>
2019-09-02 12:41:58 -0400

    Add files via upload

--- python_finance_fundamentals/Portfolio-Optimization.py
+++ python_finance_fundamentals/Portfolio-Optimization.py
@@ -101,3 +101,57 @@ plt.xlabel('Volatility')
 plt.ylabel('Return')
 
 plt.scatter(max_sr_vol, max_sr_ret, c='red', s=50, edgecolors='black')
+
+
+def get_ret_vol_sr(weights):
+    weights = np.array(weights)
+    ret = np.sum(log_ret.mean() * weights) * 252
+    vol = np.sqrt(np.dot(weights.T,np.dot(log_ret.cov()*252, weights)))
+    sr = ret/vol
+    return np.array([ret,vol,sr])
+
+from scipy.optimize import minimize
+
+def neg_sharpe(weights):
+    return get_ret_vol_sr(weights)[2] * -1
+
+def check_sum(weights):
+    # return 0 if the sum of the weights is 1
+    return np.sum(weights) - 1
+
+cons = ({'type':'eq','fun':check_sum})
+
+bounds = ((0,1),(0,1),(0,1),(0,1))
+
+initial_guess = [0.25,0.25,0.25,0.25]
+
+optimal_results = minimize(neg_sharpe,initial_guess,method='SLSQP', bounds=bounds,
+                           constraints=cons)
+optimal_results
+optimal_results.x
+get_ret_vol_sr(optimal_results.x)
+
+# efficient frontier
+frontier_y = np.linspace(0,0.3,100)
+def minimize_volatility(weights):
+    return get_ret_vol_sr(weights)[1]
+
+frontier_volatility = []
+
+for possible_return in frontier_y:
+    cons = ({'type':'eq', 'fun':check_sum},
+            {'type':'eq', 'fun':lambda w: get_ret_vol_sr(w)[0]-possible_return})
+    
+    result = minimize(minimize_volatility, initial_guess, method='SLSQP',
+                      bounds=bounds, constraints=cons)
+    
+    frontier_volatility.append(result['fun'])
+    
+# visualize the sharpe ratio
+plt.figure(figsize=(12,8))
+plt.scatter(vol_arr, ret_arr, c=sharpe_arr, cmap='plasma')
+plt.colorbar(label='Sharpe Ratio')
+plt.xlabel('Volatility')
+plt.ylabel('Return')
+
+plt.plot(frontier_volatility, frontier_y, 'g--', linewidth=3)
